f3d4e5c1f3cb0cd7a75a539636114ab0bc5caa61
asuraxxxx <Adrianfo1129@gmail.com>
2024-11-15 00:28:14 +0800

    Allows UserPlane to move left and right

--- src/main/java/com/example/demo/LevelParent.java
+++ src/main/java/com/example/demo/LevelParent.java
@@ -116,13 +116,16 @@ public abstract class LevelParent {
 				KeyCode kc = e.getCode();
 				if (kc == KeyCode.UP) user.moveUp();
 				if (kc == KeyCode.DOWN) user.moveDown();
+				if (kc == KeyCode.LEFT) user.moveLeft();
+				if (kc == KeyCode.RIGHT) user.moveRight();
 				if (kc == KeyCode.SPACE) fireProjectile();
 			}
 		});
 		background.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent e) {
 				KeyCode kc = e.getCode();
-				if (kc == KeyCode.UP || kc == KeyCode.DOWN) user.stop();
+				if (kc == KeyCode.UP || kc == KeyCode.DOWN) user.stopVerticalMovement();
+				if (kc == KeyCode.LEFT || kc == KeyCode.RIGHT) user.stopHorizontalMovement();
 			}
 		});
 		root.getChildren().add(background);
