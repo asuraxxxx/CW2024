91edee845e7af51c5fb1064a3df63cbbba597c36
asuraxxxx <Adrianfo1129@gmail.com>
2024-12-12 17:35:52 +0800

    Added JavaDoc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,8 +5,16 @@ module com.example.demo {
     requires javafx.media;
 	requires javafx.swing;
 
+
     opens com.example.demo.ui.images to javafx.fxml;
     exports com.example.demo.controller;
-    
+    exports com.example.demo.actors;
+    exports com.example.demo.audio;
+    exports com.example.demo.factories;
+    exports com.example.demo.levels;
+    exports com.example.demo.managers;
+    exports com.example.demo.ui;
+    exports com.example.demo.strategies.movement;
+    exports com.example.demo.strategies.projectile;
     
 }
